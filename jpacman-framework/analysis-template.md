f877aeddb7f21713a0c08d594bf4bcef0fdb0009
Heilan4 <olan.heinen@student.unamur.be>
2025-10-21 08:37:27 +0200

    tp2

--- jpacman-framework/analysis-template.md
+++ jpacman-framework/analysis-template.md
@@ -1,9 +1,9 @@
 # Specification-based Testing
 
 ## 1. Goal, inputs and outputs
-- Goal:
-- Input domain:
-- Output domain:
+- Goal: prochaine position de Clyde
+- Input domain: map, joueur, position de Clyde
+- Output domain: direction
 
 ## 2. Explore the program (if needed)
 
@@ -12,11 +12,56 @@
 ### Input partitions
 
 #### Individual inputs
-
+- Distance partitions:
+    - D1: Clyde is within 8 grid spaces of Pac-man (distance < 8)
+    - D2: Clyde is beyond 8 grid spaces of Pac-man (distance > 8)
+    - D3: Clyde is at exactly 8 grid spaces of Pac-man (distance = 8)
+    - 
+- Obstacle direction partitions:
+  - O1: Path of Clyde is free
+  - O2: Path of Clyde is blocked
+  - O3: Clyde has multiple valid moves
 #### Combinations of input values
+- Distance < 8 & path free
+- Distance < 8 & path blocked
+- Distance < 8 & multiple moves
 
+- Distance > 8 & path free
+- Distance > 8 & path blocked
+- Distance > 8 & multiple moves
+
+- Distance = 8 & path free
+- Distance = 8 & path blocked
+- Distance = 8 & multiple moves
 ### Output partitions
-
+- Empty direction
+- Direction towards Pac-man
+- Direction away from Pac-man
 ## 4. Identify boundaries
+- Not valid cases:
+  - Multiple Clydes on the map
+  - Multiple Pac-man on the map
+  - Clyde is on Pac-man
+  - Clyde does not have a square partition
+  - Pac-man does not have a square partition
 
+- Pac-man at the edge of the board partition
+- Pac-man goes from one edge of the map to the other
 ## 5. Select test cases
+- Distance < 8:
+  - T1: Path free => Direction away from Pac-man
+  - T2: Path blocked => Empty direction
+  - T3: multiple moves => Direction away from Pac-man
+- Distance > 8:
+  - T4: Path free => Direction towards from Pac-man
+  - T5: Path blocked => Empty direction
+  - T6: multiple moves => Direction towards from Pac-man
+- Distance = 8:
+  - T7: Path free => Empty direction
+  - T8: Path blocked => Empty direction
+  - T9: multiple moves => Empty direction
+
+- Boundaries:
+  - T10: Multiple Clydes on the map
+  - T11: Multiple Pac-man on the map
+  - T12: Clyde is on Pac-man
---
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -0,0 +1,84 @@
+package nl.tudelft.jpacman.npc.ghost;
+
+import com.google.common.collect.Lists;
+import nl.tudelft.jpacman.board.*;
+import nl.tudelft.jpacman.level.*;
+import nl.tudelft.jpacman.npc.Ghost;
+import nl.tudelft.jpacman.sprite.PacManSprites;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ClydeTest {
+    private Clyde clyde;
+    private final PacManSprites sprites = new PacManSprites();
+    private final GhostFactory ghostFactory = new GhostFactory(sprites);
+    private PlayerFactory playerFactory = new PlayerFactory(sprites);
+    private final BoardFactory boardFactory = new BoardFactory(sprites);
+    private final LevelFactory levelFactory = new LevelFactory(sprites,ghostFactory);
+    private final GhostMapParser ghostMapParser = new GhostMapParser(levelFactory,boardFactory,ghostFactory);
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void noPacman() {
+        char[][] map = {"############".toCharArray(), "#P         C#".toCharArray(), "############".toCharArray()};
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class,ghostMapParser.parseMap(map).getBoard());
+
+        assert clyde != null;
+        assert clyde.hasSquare();
+        Optional<Direction> nextMove = clyde.nextAiMove();
+        assertThat(nextMove.isEmpty()).isEqualTo(true);
+    }
+
+    @Test
+    void noClyde() {
+        char[][] map = {"############".toCharArray(), "#P          #".toCharArray(), "############".toCharArray()};
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class,ghostMapParser.parseMap(map).getBoard());
+
+        assertThat(clyde).isNull();
+    }
+
+    @Test
+    void freeWayLongRange() {
+        List<String> map = Arrays.asList(
+            "#############",
+            "P           C",
+            "###########  "
+        );
+        Level lvl = ghostMapParser.parseMap(map);
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, lvl.getBoard());
+        Player player = playerFactory.createPacMan();
+        lvl.registerPlayer(player);
+        assert clyde != null;
+        Optional<Direction> nextMove = clyde.nextAiMove();
+        System.out.println(nextMove);
+        assertThat(nextMove.get()).isEqualTo(Direction.valueOf("WEST"));
+    }
+
+    @Test
+    void freeWayShortRange() {
+        List<String> map = Arrays.asList(
+            "#############",
+            "######P    C ",
+            "###########  "
+        );
+        Level lvl = ghostMapParser.parseMap(map);
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, lvl.getBoard());
+        Player player = playerFactory.createPacMan();
+        lvl.registerPlayer(player);
+        assert clyde != null;
+        Optional<Direction> nextMove = clyde.nextAiMove();
+        assertThat(nextMove.get()).isEqualTo(Direction.valueOf("EAST"));
+    }
+}
